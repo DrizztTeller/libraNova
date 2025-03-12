9acced0f2df1d1563fb9a211a5720da804d460f2
Mohammad ABDUL <abdul.opto@gmail.com>
2025-03-12 12:41:55 +0100

    Ajout du champ created_at à l'entité Novel avec les méthodes d'accès correspondantes

--- src/Entity/Novel.php
+++ src/Entity/Novel.php
@@ -70,6 +70,9 @@ class Novel
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $isbn = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
     public function __construct()
     {
         $this->tags = new ArrayCollection();
@@ -306,6 +309,18 @@ class Novel
     public function setIsbn(?string $isbn): static
     {
         $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
 
         return $this;
     }
