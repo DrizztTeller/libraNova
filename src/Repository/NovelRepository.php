b3b4e240114ee22c2a0ac079cbfb39a3e92a92d0
Mohammad ABDUL <abdul.opto@gmail.com>
2025-03-12 15:04:44 +0100

    Suppression de la méthode findByProperty dans NovelRepository et nettoyage du code

--- src/Repository/NovelRepository.php
+++ src/Repository/NovelRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Novel;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+// use Doctrine\DBAL\Types\Types;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Novel>
@@ -30,6 +31,38 @@ class NovelRepository extends ServiceEntityRepository
     //            ->getResult()
     //        ;
     //    }
+
+    // public function findByProperty(string $property, string $operator, mixed $filter): array
+    // {
+    //     $allowedProperties = ['title', 'author', 'created_at', 'published_at', 'likes'];
+    //     if (!in_array($property, $allowedProperties)) {
+    //         throw new \InvalidArgumentException("Propriété non autorisée.");
+    //     }
+    
+    //     $allowedOperators = ['=', '!=', '<', '>', '<=', '>='];
+    //     if (!in_array($operator, $allowedOperators)) {
+    //         throw new \InvalidArgumentException("Opérateur non autorisé.");
+    //     }
+    
+    //     $qb = $this->createQueryBuilder('n');
+    
+    //     if ($property === 'likes') {
+    //         // Gestion du COUNT(likes)
+    //         $qb->leftJoin('n.likes', 'u')
+    //             ->groupBy('n.id')
+    //             ->having("COUNT(u.id) $operator :filter")
+    //             ->setParameter('filter', $filter);
+    //     } else {
+    //         // Gestion des autres propriétés
+    //         $type = in_array($property, ['created_at', 'published_at']) ? Types::DATETIME_IMMUTABLE : Types::STRING;
+    //         $qb->where("n.$property $operator :filter")
+    //             ->setParameter('filter', $filter, $type);
+    //     }
+    
+    //     return $qb->orderBy("n.$property", 'DESC')
+    //         ->getQuery()
+    //         ->getResult();
+    // }
 
     //    public function findOneBySomeField($value): ?Novel
     //    {
