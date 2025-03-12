2471091000baf17d679b8a8d6766384435adf1a0
Tarek <tarek_lamarti@live.fr>
2025-03-12 16:57:41 +0100

    Enhance NovelFixture: implement slug generation, randomize author selection, and improve tag assignment logic

--- src/DataFixtures/NovelFixture.php
+++ src/DataFixtures/NovelFixture.php
@@ -8,49 +8,112 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
-class NovelFixture extends Fixture //implements DependentFixtureInterface
+class NovelFixture extends Fixture implements DependentFixtureInterface
 {
+    private SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR'); // Chargement de Faker
+        
+        $authors = [
+            'J.K. Rowling',
+            'Stephen King',
+            'George R.R. Martin',
+            'Haruki Murakami',
+            'Margaret Atwood',
+            'Neil Gaiman',
+            'Chimamanda Ngozi Adichie',
+            'Jane Austen',
+            'Gabriel García Márquez',
+            'Toni Morrison'
+        ];
 
-             // Créer des livres
-        for ($i = 0; $i < 50; $i++) {
+        // Liste des titres selon le type
+        $titlePrefixes = [
+            'Roman' => 'Bridget Jones - ',
+            'Science-Fiction' => 'Star Wars - ',
+            'Biographie' => 'La Vie de ',
+            'Histoire' => 'Histoire de ',
+            'Jeunesse' => 'Les Aventures de ',
+            'Technique' => 'Guide de ',
+            'Manga' => 'One Piece - ',
+            'Adulte'
+        ];
+
+        // Récupérer tous les tags
+        $tagRepo = $manager->getRepository(Tag::class);
+        $allTags = $tagRepo->findAll();
+        
+             // Créer 50 livres
+        for ($i = 0; $i < 20; $i++) {
             $novel = new Novel();
-            $novel->setName($faker->sentence(3));
-            $novel->setDescription($faker->paragraphs());
-            $novel->setAbstract($faker->paragraphs(50));
-            $novel->setPublicationDate($faker->dateTimeBetween('-30 years', 'now'));
-            $novel->setPageCount($faker->numberBetween(50, 1200));
-            $novel->setPublishedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeThisDecade()));
-            $novel->setIsbn($faker->isbn13());
-            
-            // Attribuer un auteur aléatoire
-            $authorIndex = $faker->numberBetween(0, 9);
-            $novel->setAuthor($this->getReference('author_' . $authorIndex));
 
-            // Ajouter entre 1 et 3 tags aléatoires
+            //Selection de tags
             $tagCount = $faker->numberBetween(1, 3);
-            $tagIndexes = $faker->randomElements(range(0, 7), $tagCount);
+            $selectedTags = $faker->randomElements($allTags, $tagCount);
+
+            // Déterminer un titre en fonction du premier tag
+            $firstTag = $selectedTags[0];
+            $tagName = $firstTag->getName();
+            $prefix = $titlePrefixes[$tagName] ?? '';
+            $title = $prefix . $faker->words(3, true);
+
+
+            $novel->setName($title);
+            $novel->setAuthor($faker->randomElement($authors));  // Attribuer un auteur aléatoire
+            $novel->setAbstract($faker->paragraphs(3, true));
+            $novel->setIsPublished(true);
+            $novel->setReleasedAt($faker->dateTimeThisDecade());
+            $novel->setCreatedAt(new \DateTimeImmutable());
+
+
+            // Images et fichiers fictifs
             
-            foreach ($tagIndexes as $tagIndex) {
-                $novel->addTag($this->getReference('tag_' . $tagIndex));
+            // Images avec des IDs fixes (choisies pour ressembler à des couvertures de livres)
+            $bookCoverIds = [20, 24, 42, 67, 101, 180, 240, 251, 292, 331, 373, 384];
+            $randomBookCoverId = $bookCoverIds[array_rand($bookCoverIds)];
+            $novel->setPic('https://picsum.photos/id/' . $randomBookCoverId . '/800/600');
+            $novel->setFile('book_' . $faker->numberBetween(1, 20) . '.pdf');
+        
+            
+            // Slug et référence
+            $slug = $this->slugger->slug($title)->lower();
+            $novel->setSlug($slug);
+            $novel->setRef('NOVEL-' . $faker->unique()->numerify('######'));
+            $novel->setIsbn($faker->isbn13());
+            $novel->setIsForAdult($faker->boolean(20));
+            // Ajouter les tags au roman
+            foreach ($selectedTags as $tag) {
+                $novel->addTag($tag);
             }
             
-            
             $manager->persist($novel);
-
+        }
         $manager->flush();
         
         }
-    }
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
-            AuthorFixture::class,
-            ClientFixture::class,
             TagFixture::class,
+            UserFixture::class,
         ];
     }
 }
+            // $authorIndex = $faker->numberBetween(0, 9);
+            // $novel->setAuthor($this->getReference('author_' . $authorIndex));
+
+            // Ajouter entre 1 et 3 tags aléatoires
+            // $tagCount = $faker->numberBetween(1, 3);
+            // $tagIndexes = $faker->randomElements(range(0, 7), $tagCount);
+            
+            // foreach ($tagIndexes as $tagIndex) {
+            //     $novel->addTag($this->getReference('tag_' . $tagIndex));
+            // }
