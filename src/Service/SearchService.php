f2c561fba551b6018646064d77ac76ad5e94582c
Mohammad ABDUL <abdul.opto@gmail.com>
2025-03-12 15:23:16 +0100

    Refactor SearchService pour améliorer la lisibilité et la modularité du code de recherche

--- src/Service/SearchService.php
+++ src/Service/SearchService.php
@@ -8,105 +8,130 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class SearchService
 {
-  private EntityManagerInterface $entityManager;
+    private EntityManagerInterface $entityManager;
+    private const ALLOWED_PROPERTIES = ['title', 'author', 'created_at', 'published_at', 'likes', 'tags'];
 
-  public function __construct(EntityManagerInterface $entityManager)
-  {
-    $this->entityManager = $entityManager;
-  }
-
-  public function search(QueryBuilder $queryBuilder, array $criteria): array
-  {
-    // Liste des propriétés autorisées
-    $allowedProperties = ['title', 'author', 'created_at', 'published_at', 'likes', 'tags'];
-
-    if (!empty($criteria['property']) && in_array($criteria['property'], $allowedProperties)) {
-      // Cas particulier pour les tags (ManyToMany)
-      if ($criteria['property'] === 'tags' && !empty($criteria['filter'])) {
-        $queryBuilder->join('e.tag', 't')
-          ->andWhere('t.id IN (:tags)')
-          ->setParameter('tags', $criteria['filter']);
-      } else {
-        $queryBuilder->andWhere("e.{$criteria['property']} < :filter")
-          ->setParameter('filter', $criteria['filter'])
-          ->orderBy("e.{$criteria['property']}", 'DESC');
-      }
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
     }
 
-    // Recherche par titre
-    if (!empty($criteria['title'])) {
-      $queryBuilder->andWhere('e.title LIKE :title')
-        ->setParameter('title', '%' . $criteria['title'] . '%');
+    public function search(QueryBuilder $queryBuilder, array $criteria): array
+    {
+        $this->applyPropertyFilter($queryBuilder, $criteria);
+        $this->applyTitleFilter($queryBuilder, $criteria);
+        $this->applyAuthorFilter($queryBuilder, $criteria);
+        $this->applyDateFilters($queryBuilder, $criteria);
+        $this->applyTagFilters($queryBuilder, $criteria);
+        $this->applyExcludedTagsFilter($queryBuilder, $criteria);
+        $this->applyLikesFilter($queryBuilder, $criteria);
+        $this->applyPublicationStatusFilter($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $criteria);
+
+        return $queryBuilder->getQuery()->getResult();
     }
 
-    // Recherche par auteur
-    if (!empty($criteria['author'])) {
-      $queryBuilder->andWhere('e.author LIKE :author')
-        ->setParameter('author', '%' . $criteria['author'] . '%');
+    private function applyPropertyFilter(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['property']) && in_array($criteria['property'], self::ALLOWED_PROPERTIES, true)) {
+            if ($criteria['property'] === 'tags' && !empty($criteria['filter'])) {
+                $queryBuilder->join('e.tag', 't')
+                    ->andWhere('t.id IN (:tags)')
+                    ->setParameter('tags', $criteria['filter']);
+            } else {
+                $queryBuilder->andWhere("e.{$criteria['property']} < :filter")
+                    ->setParameter('filter', $criteria['filter'])
+                    ->orderBy("e.{$criteria['property']}", 'DESC');
+            }
+        }
     }
 
-    // Filtrer par date de création (moins d'une semaine)
-    if (!empty($criteria['created_within_week']) && $criteria['created_within_week'] === true) {
-      $oneWeekAgo = new DateTimeImmutable('-1 week');
-      $queryBuilder->andWhere('e.created_at >= :oneWeekAgo')
-        ->setParameter('oneWeekAgo', $oneWeekAgo);
+    private function applyTitleFilter(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['title'])) {
+            $queryBuilder->andWhere('e.title LIKE :title')
+                ->setParameter('title', '%' . $criteria['title'] . '%');
+        }
     }
 
-    // Filtrer par date de publication (moins d'une semaine)
-    if (!empty($criteria['published_within_week']) && $criteria['published_within_week'] === true) {
-      $oneWeekAgo = new DateTimeImmutable('-1 week');
-      $queryBuilder->andWhere('e.published_at >= :oneWeekAgo')
-        ->setParameter('oneWeekAgo', $oneWeekAgo);
+    private function applyAuthorFilter(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['author'])) {
+            $queryBuilder->andWhere('e.author LIKE :author')
+                ->setParameter('author', '%' . $criteria['author'] . '%');
+        }
     }
 
-    // Filtrer par tags
-    if (!empty($criteria['tags'])) {
-      $queryBuilder->join('e.tag', 't')
-        ->andWhere('t.id IN (:tags)')
-        ->setParameter('tags', $criteria['tags']);
+    private function applyDateFilters(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        $oneWeekAgo = new DateTimeImmutable('-1 week');
 
-      if (!empty($criteria['matchType']) && $criteria['matchType'] === 'all') {
-        $queryBuilder->groupBy('e.id')
-          ->having('COUNT(t.id) = :tagCount')
-          ->setParameter('tagCount', count($criteria['tags']));
-      }
+        if (!empty($criteria['created_within_week'])) {
+            $queryBuilder->andWhere('e.created_at >= :createdOneWeekAgo')
+                ->setParameter('createdOneWeekAgo', $oneWeekAgo);
+        }
+
+        if (!empty($criteria['published_within_week'])) {
+            $queryBuilder->andWhere('e.published_at >= :publishedOneWeekAgo')
+                ->setParameter('publishedOneWeekAgo', $oneWeekAgo);
+        }
     }
 
-    // Exclure certains tags
-    if (!empty($criteria['excludeTags'])) {
-      $rootEntity = $queryBuilder->getRootEntities()[0];
+    private function applyTagFilters(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['tags'])) {
+            $queryBuilder->join('e.tag', 't')
+                ->andWhere('t.id IN (:tags)')
+                ->setParameter('tags', $criteria['tags']);
 
-      $subQuery = $this->entityManager->createQueryBuilder()
-        ->select('sub.id')
-        ->from($rootEntity, 'sub')
-        ->leftJoin('sub.tag', 'st')
-        ->where('st.id IN (:excludeTags)');
-
-      $queryBuilder->andWhere($queryBuilder->expr()->notIn('e.id', $subQuery->getDQL()))
-        ->setParameter('excludeTags', $criteria['excludeTags']);
+            if (!empty($criteria['matchType']) && $criteria['matchType'] === 'all') {
+                $queryBuilder->groupBy('e.id')
+                    ->having('COUNT(DISTINCT t.id) = :tagCount')
+                    ->setParameter('tagCount', count($criteria['tags']));
+            }
+        }
     }
 
-    // Filtrer les livres populaires par nombre de likes
-    if (!empty($criteria['likes']) && is_numeric($criteria['likes'])) {
-      $queryBuilder->leftJoin('e.likes', 'l')
-        ->groupBy('e.id')
-        ->having('COUNT(l.id) >= :likes')
-        ->setParameter('likes', $criteria['likes']);
+    private function applyExcludedTagsFilter(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['excludeTags'])) {
+            $rootEntity = $queryBuilder->getRootEntities()[0];
+
+            $subQuery = $this->entityManager->createQueryBuilder()
+                ->select('sub.id')
+                ->from($rootEntity, 'sub')
+                ->leftJoin('sub.tag', 'st')
+                ->where('st.id IN (:excludeTags)');
+
+            $queryBuilder->andWhere($queryBuilder->expr()->notIn('e.id', $subQuery->getDQL()))
+                ->setParameter('excludeTags', $criteria['excludeTags']);
+        }
     }
 
-    // Filtrer par état de publication
-    if (!empty($criteria['is_published'])) {
-      $queryBuilder->andWhere('e.is_published IN (:statuses)')
-        ->setParameter('statuses', $criteria['is_published']);
+    private function applyLikesFilter(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['likes']) && is_numeric($criteria['likes'])) {
+            $queryBuilder->leftJoin('e.likes', 'l')
+                ->groupBy('e.id')
+                ->having('COUNT(l.id) >= :likes')
+                ->setParameter('likes', $criteria['likes']);
+        }
     }
 
-    // Trier les résultats
-    if (!empty($criteria['orderBy']) && !empty($criteria['orderDirection'])) {
-      $queryBuilder->orderBy('e.' . $criteria['orderBy'], $criteria['orderDirection']);
-    } else {
-      $queryBuilder->orderBy('e.created_at', 'DESC');
+    private function applyPublicationStatusFilter(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (isset($criteria['is_published'])) {
+            $queryBuilder->andWhere('e.is_published = :isPublished')
+                ->setParameter('isPublished', $criteria['is_published']);
+        }
     }
 
-    return $queryBuilder->getQuery()->getResult();
-  }
+    private function applySorting(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (!empty($criteria['orderBy']) && !empty($criteria['orderDirection'])) {
+            $queryBuilder->orderBy('e.' . $criteria['orderBy'], $criteria['orderDirection']);
+        } else {
+            $queryBuilder->orderBy('e.created_at', 'DESC');
+        }
+    }
 }
