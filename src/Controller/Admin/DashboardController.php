6a6b7639bc01a9bac5ce9c77719b947f91af9987
Tarek <tarek_lamarti@live.fr>
2025-03-13 14:25:45 +0100

    Ajout du DashboardController et des routes associées pour l'administration, ainsi qu'un template de dashboard.

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\LoginHistory;
+use App\Entity\Novel;
+use App\Entity\RentingHistory;
+use App\Entity\Tag;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    public function index(): Response
+    {
+        // return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // 1.1) If you have enabled the "pretty URLs" feature:
+        // return $this->redirectToRoute('admin_user_index');
+        //
+        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirectToRoute('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('LibraNova');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Menu', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+
+        //Ici je créer les liens vers mes entités pour le menu admin
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Livres', 'fas fa-book-quran',  NovelCrudController::getEntityFqcn());
+        yield MenuItem::linkToCrud('Locations', 'fas fa-clock-rotate-left', RentingHistory::class);
+        yield MenuItem::linkToCrud('Connexions utilisateurs', 'fas fa-font-awesome', LoginHistory::class);
+        yield MenuItem::linkToCrud('Tags', 'fas fa-icons', Tag::class);
+        yield MenuItem::section('<hr>');
+        yield MenuItem::linkToRoute('Retour au site', 'fas fa-person-through-window','home' );
+    }
+}
