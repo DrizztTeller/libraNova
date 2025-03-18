29c9ae103e6a6fe19a77ba4e4c79e71e52bdf679
Tarek <tarek_lamarti@live.fr>
2025-03-18 12:35:18 +0100

    Refactorisation de NovelFixture: ajout de la gestion des dépendances et amélioration de la récupération des tags

--- src/DataFixtures/NovelFixture.php
+++ src/DataFixtures/NovelFixture.php
@@ -12,12 +12,21 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class NovelFixture extends Fixture implements DependentFixtureInterface
 {
+    public function getDependencies(): array
+    {
+        return [
+            TagFixture::class,
+            UserFixture::class,
+        ];
+    }
     private SluggerInterface $slugger;
 
     public function __construct(SluggerInterface $slugger)
     {
         $this->slugger = $slugger;
     }
+
+    
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR'); // Chargement de Faker
@@ -44,12 +53,24 @@ class NovelFixture extends Fixture implements DependentFixtureInterface
             'Jeunesse' => 'Les Aventures de ',
             'Technique' => 'Guide de ',
             'Manga' => 'One Piece - ',
-            'Adulte'
+            'Adulte' => '50 shades of'
         ];
 
-        // Récupérer tous les tags
-        $tagRepo = $manager->getRepository(Tag::class);
-        $allTags = $tagRepo->findAll();
+        // // Récupérer tous les tags
+        // $tagRepo = $manager->getRepository(Tag::class);
+        // $allTags = $tagRepo->findAll();
+
+        // Récupérer une liste de tags via les références créées dans TagFixture
+        $allTags = [];
+        for ($i = 0; $i < 8; $i++) { // Pour 8 tags
+            try {
+                $tag = $this->getReference('tag_' . $i, Tag::class);
+                $allTags[] = $tag;
+            } catch (\Exception $e) {
+                // Gérer l'exception ou l'ignorer silencieusement
+                dump("Tag référence 'tag_" . $i . "' non trouvé");
+            }
+        }
         
              // Créer 50 livres
         for ($i = 0; $i < 20; $i++) {
@@ -59,16 +80,20 @@ class NovelFixture extends Fixture implements DependentFixtureInterface
             $tagCount = $faker->numberBetween(1, 3);
             $selectedTags = $faker->randomElements($allTags, $tagCount);
 
+            foreach ($selectedTags as $tag) {
+                $novel->addTag($tag); // Liaison des tags avec le roman
+            }
+
             // Déterminer un titre en fonction du premier tag
-            $firstTag = $selectedTags[0];
-            $tagName = $firstTag->getName();
-            $prefix = $titlePrefixes[$tagName] ?? '';
-            $title = $prefix . $faker->words(3, true);
+            $firstTag = $selectedTags[0]; // Obtenir le premier tag
+            $tagNames = $firstTag->getName(); // Obtenir le nom du premier tag
+            $prefix = $titlePrefixes[$tagNames] ?? ''; // Utiliser le prefixe par défaut
+            $title = $prefix . $faker->words(3, true); // Attribuer un titre aléatoire
 
             $novel->setTitle($title);
             $novel->setAuthor($faker->randomElement($authors));  // Attribuer un auteur aléatoire
             $novel->setAbstract($faker->paragraphs(3, true));
-            $novel->setIsPublished(true);
+            $novel->setIsPublished(true, 70);
             $novel->setReleasedAt($faker->dateTimeThisDecade());
             $novel->setCreatedAt(new \DateTimeImmutable());
 
@@ -78,7 +103,7 @@ class NovelFixture extends Fixture implements DependentFixtureInterface
             // Images avec des IDs fixes (choisies pour ressembler à des couvertures de livres)
             $bookCoverIds = [20, 24, 42, 67, 101, 180, 240, 251, 292, 331, 373, 384];
             $randomBookCoverId = $bookCoverIds[array_rand($bookCoverIds)];
-            $novel->setPic('https://picsum.photos/id/' . $randomBookCoverId . '/800/600');
+            $novel->setPicUrl('https://picsum.photos/id/' . $randomBookCoverId . '/800/600');
             // TODO remplacer par réel fichier
             $novel->setFile('book_' . $faker->numberBetween(1, 20) . '.pdf');
         
@@ -88,11 +113,8 @@ class NovelFixture extends Fixture implements DependentFixtureInterface
             $novel->setSlug($slug);
             $novel->setRef($slug . '-' . $faker->unique()->numerify('######'));
             $novel->setIsbn($faker->isbn13());
-            $novel->setIsForAdult($faker->boolean(20));
-            // Ajouter les tags au roman
-            foreach ($selectedTags as $tag) {
-                $novel->addTag($tag);
-            }
+            $novel->setIsForAdult($faker->boolean(30));
+
             // TODO si is_for_adult true faut absolument tag adulte
             
             $manager->persist($novel);
@@ -100,13 +122,7 @@ class NovelFixture extends Fixture implements DependentFixtureInterface
         $manager->flush();
         
         }
-    public function getDependencies(): array
-    {
-        return [
-            TagFixture::class,
-            UserFixture::class,
-        ];
-    }
+
 }
             // $authorIndex = $faker->numberBetween(0, 9);
             // $novel->setAuthor($this->getReference('author_' . $authorIndex));
